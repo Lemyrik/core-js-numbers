18bfd64579c5cffcdd290b296117fd238cb6aeed
Malyshka <lesyamalyshko@gmail.com>
2024-12-09 19:12:09 +0300

    feat: resolve isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -198,8 +198,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let count = 0;
+  for (let i = 2; i <= n; i += 1) {
+    count += n % i === 0 ? 1 : 0;
+  }
+  return count === 1;
 }
 
 /**
